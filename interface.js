ef4d71c751d8bda5b833a7c05828c592922ee0a4
trhi <terhim@gmail.com>
2022-04-01 17:13:50 +0100

    Add four new snippets (mosquito, ocean, wind and kaipaa song, update thoughts.json, remove a few thoughts, clean up index.html, interface.js, transplanted.js, add comments to code, add acknowledgments, artist statemetn

--- interface.js
+++ interface.js
@@ -1,23 +1,74 @@
-var mitae = [], miksi = [], miten = [], missae = [], rihmasto = [], juuret = [], kaipuu = [], keywords = [], structure = [];
+/*
+*  Transplanted (2021 - 2022 -?)
+*  by Terhi Marttila
+*
+*  Transplanted is about missing past and future/current homes.
+*  A reflection on the ambivalent emotions caused by being transplanted.
+*
+*  Project credits and acknowledgments:
+*
+*  Terhi Marttila: concept, programming, texts, voice (acting?)
+*  Tero Marttila: the core logics of the code in function play() which I copied from Give me a Reason (2016)
+*  Diogo Cocharro: for borrowing the recording equipment (audio interface, microphone and stand)
+*  Miikka Laihinen: nokturno.fi editor 2022 onwards for all support related to
+*  publishing Transplanted in nokturno. I especially thank Miikka for the push to
+*  include english subtitles, because the subtitles (and the speech input in English)
+*  make the work accessible to a broader audience
+*  Virpi Vairinen: the previous editor of nokturno.fi for initially accepting
+*  Transplanted for publication in early December of 2021
+*
+*  Satu Miettinen, Heidi Pietarinen, Melanie Sarantou, Tomi Knuutila, Outi Valanto:
+*  Such a huge thanks to Satu Miettinen for inviting me to participate in the
+*  group exhibition. The conversations that unfolded over the messaging service for the
+*  Growth, Death and Decay exhibition were absolutely crucial towards the development
+*  of Transplanted. I was very inspired by the work of my artist collegues, who
+*  were working with plants and with roots, Melanie was succesfully planting
+*  sunflower seeds in Lapland in moist wool (!!!) and that's one of the reasons
+*  why I started thinking of migratory movement through the concept of
+*  transplanting, roots, the rhizome, and so on. For me, this exchange,
+*  of work in progress and ideas amongst the participating artists, was in itself
+*  a central methodological approach for my artistic research. Besides the
+*  intellectual exchange, the collaboration involved lots of laughs and humor,
+*  which was much needed especially in early 2021 during the second COVID lockdown.
+*
+*  This work was initially created for the Growth, Death and Decay -exhibition
+*  which took place on 9. - 25.11.2021 in Galleria Hämärä, Rovaniemi, Lapland
+*  (University of Lapland)
+*
+*  Additional elements were created for the publication on Nokturno.fi in March of 2022
+*  including: English subtitles, possibilty to interact/speak in English,
+*  the ∞-mode, new recordings (mosquito, ocean, wind, 'kaipaan'-"song"),
+*  refactoring and rewriting some of the code, code comments.
+*
+*  This work is part of the doctoral dissertation of Terhi Marttila at the University
+*  of Porto, Portugal (digital media) which will be defended in early 2022.
+*  The doctoral dissertation is available at terhimarttila.com/migration-as-movement
+*  or somehwere at github.com/trhi
+*
+*  This work was financially supported by: Operation NORTE-08-5369-FSE-000049
+*  co funded by the European Social Fund (FSE) through
+*  NORTE 2020 - Programa Operacional Regional do NORTE.
+*
+*  The h-ear-t image and audio recordings are copyright Terhi Marttila and are
+*  not allowed to be used in any way, shape or form ever anywhere. Not for
+*  non-commercial purposes, not for commercial purposes. Please contact me if you
+*  want to invite the work for exhibition etc.
+*
+*  Nor are you allowed to copy this work as a whole and present it on a website
+*  without my consent.
+*
+*  The code itself and the concept, ie: organising snippets under structures,
+*  then randomly selecting structures and then playing back audio is licenced
+*  under CC BY 4.0 - ie. you are welcome to copy any of this code and use it in
+*  whatever way you want. I appreciate if you mention my work as an inspiration
+*  or a reference, and I will be happy to hear of any work that is in part inspired
+*  by or based on my work.
+*
+*/
+
+var canvas, heartButton, speakingToMyHeart, infoButton, finnishButton, englishButton, infinityButton, url, language, thoughts;
+var keywords = [], structure = [];
 var infinity;
-var counter = 0;
-var instructions;
-var prompts = [ //ended up not using these
-      'mitä kaipaan ? ',
-      'voiko ihminen juurtua ? ',
-      'miksi olen täällä ? ',
-      'miten ihminen juurtuu ? ',
-      'mitä kaipuu on ? ',
-      'miksi viihdyn täällä niin hyvin ? ',
-      'miksi muutin pois ? ',
-      'missä elämä on ? ',
-      'miksi olen tässä ? ',
-      'missä on juuret ? ',
-      'miksi kaivata paikaan, jossa ei ole ? ',
-      'olenko rihmasto ? ',
-      'missä on minun paikka ? ',
-      'miksi olen täällä ? '
-    ];
 
 function doInterface(){
 
@@ -26,11 +77,12 @@ function doInterface(){
   canvas.id('canvas');
   background("transparent");
 
+  //infoButton to display instructions on how to use the piece:
   infoButton = createButton('i');
   infoButton.addClass('infobutton');
 
-
-  //infinityButton: infinite musing by Transplanted
+  //infinityButton for infinite musing by Transplanted:
+  infinity = false; //by default, Transplanted is *not* in infinity mode
   infinityButton = createButton('∞');
   infinityButton.id("select-infinity");
   infinityButton.mouseOver( () => {
@@ -49,36 +101,32 @@ function doInterface(){
     }
   });
 
-  infinity = false;
   infinityButton.mousePressed( () => {
 
     if(!infinity){
-      //infinityButton.innerText = '8';
-      //document.getElementById(select-infinity).textContent = '8';
-      //infinityButton.html('8');
-
+      $("#answers").stop(true, true).empty().hide();
+      //when infinityButton is selected, change background to pink (selected):
       infinityButton.style("background: #ff1e8c");
       infinityButton.style('filter', 'drop-shadow(0 0 0.5rem #ff1e8c');
       infinity = true;
       listener.stop();
 
-      //disable heartButton:
+      //while infinity mode is on, person cannot speak to Transplanted:
       heartButton.mousePressed( () => console.log("disbaled heartButton") );
 
-      if(infinity){
+      if(infinity){ //once infinity mode is turned on, museAboutBeingTransplanted("infinity"):
         museAboutBeingTransplanted("infinity");
       }
 
     }else{
-    //turn off infinityButton
+    //turn off infinityButton:
       infinityButton.style("background: white");
       infinityButton.style("filter: none");
       infinity = false;
-      //console.log("infinity is false");
       $('#audio').empty();
-      $("#answers").stop(true, true).empty().hide();
+      $("#answers").stop(true, true).empty().hide(); //empty the poems
 
-      //enable heartButton:
+      //and then enable heartButton again:
       heartButton.mousePressed( () => listenToMyHeart() )
       .mouseReleased( () => stopAndClear() )
       .mouseMoved( () => stopAndClear() )
@@ -87,10 +135,10 @@ function doInterface(){
 
   });
 
-  //finnishButton: speak to Transplanted in Finnish and no transcript visible
+  //finnishButton: speak to Transplanted in Finnish, no transcript visible
   finnishButton = createButton('fi');
   finnishButton.id('select-finnish');
-  finnishButton.style("background: #1e8cff");
+  finnishButton.style("background: #1e8cff"); //it is selected by default
   finnishButton.style('filter', 'drop-shadow(0 0 0.5rem #1e8cff');
   finnishButton.mouseOver( () => {
     $("#finnish-button-info").show();
@@ -102,25 +150,21 @@ function doInterface(){
     listener.stop();
     $("#answers").stop(true, true).empty().hide();
 
-    //if(listener.lang="en-US")
-    if(language == 'en'){
+    if(language == 'en'){ //if in english atm, change to finnish:
       $("#answers").stop(true, true).empty().hide();
       language = 'fi';
-      //listener.lang="fi-FI";
-      //subtitles=false;
-      finnishButton.style("background: #1e8cff");
+      finnishButton.style("background: #1e8cff"); //change background of finnishButton to blue (selected)
       finnishButton.style('filter', 'drop-shadow(0 0 0.5rem #1e8cff');
-      englishButton.style('filter', 'none');
+      englishButton.style('filter', 'none'); //change background of englishButton to white (not selected)
       englishButton.style("background: white");
     }
   });
 
-  //englishButton: (or rather englishButton?) speak to Transplanted in English, and translated transcript visible
+  //englishButton: speak to Transplanted in English, and English translation of transcript visible
   englishButton = createButton('en');
   englishButton.id('select-english');
   englishButton.mouseOver( () => {
     $("#subtitles-button-info").show();
-
   }).mouseOut( () => {
     $("#subtitles-button-info").hide();
   });
@@ -129,33 +173,24 @@ function doInterface(){
     listener.stop();
     $("#answers").stop(true, true).empty().hide();
     if(language == 'fi'){
-    //if(listener.lang=="fi-FI"){
       language = 'en';
-      //listener.lang="en-US";
-      //subtitles = true;
-      englishButton.style("background: #FFD500");
+      englishButton.style("background: #FFD500"); //change background of englishButton to yellow (selected)
       englishButton.style('filter', 'drop-shadow(0 0 0.5rem #FFD500');
-      finnishButton.style('filter', 'none');
+      finnishButton.style('filter', 'none'); //change background of finnishButton to white (not selected)
       finnishButton.style("background: white");
     }
   });
 
-
   infoButton.mouseOver( () => {
-    //if(!subtitles){
-    if(language == 'fi'){
-      $("#instructions-fi").show();
-    //} else if (subtitles){
-    } else if (language == 'en'){
-      $("#instructions-en").show();
-    }
+      if(language == 'fi'){
+        $("#instructions-fi").show();
+      } else if (language == 'en'){
+        $("#instructions-en").show();
+      }
     })
   .mouseOut( () => {
-    //if(!subtitles){
     $('#instructions-fi').hide();
-  //} else if (subtitles){
     $("#instructions-en").hide();
-  //}
     });
 
   heartButton = createImg('assets/images/h-ear-t.png');
@@ -174,46 +209,33 @@ function stopAndClear(){
   listener.stop();
 }
 
-function listenToMyHeart() {
-  //$("#answers").stop(true, true).empty().hide();
-  $("#answers").empty();
+function listenToMyHeart() { //activated once person presses the h-ear-t:
+  $("#answers").stop(true, true).empty().hide();
   $("#questions").show();
   $("#questions").text('');
   $("#questions").css('opacity', '1');
   $("#questions").text('" 🎤  "'); // 🗣️  🎤
   heartButton.attribute('src', 'assets/images/h-ear-t.gif');
-  /*
-  if(!subtitles){
-    listener.lang = 'fi-FI';
-  } else if (subtitles){ //if subtitles is set to en, then change listener language to en
-    listener.lang = 'en-US';
-  }
-  */
-
   listener.start();
 }
 
 function museAboutBeingTransplanted(whatTheyAskedUs){
 
-  $("#answers").stop(true, true).empty().hide();
-  //stopAndClear();
-  //$("#answers").stop();
-  //$("#answers").stop(true, true).empty().show();
-  //$("#answers").show();
+  if(!infinity){
+    $("#answers").stop(true, true).empty().hide();
+  }
 
   //identify any keywords spoken:
-  //if(!subtitles){
   if(language == 'fi'){
-  keywords = whatTheyAskedUs.match(/miksi|mitä|miten|missä|tänne|tääl|tässä|siellä|paikka|järv|metsä|rihmasto|juur|kasv|maas|muut|elämä|kaip|kaiv|tunn|tunt|viih|ihmi/ig);
-//} else if (subtitles){
-} else if (language == 'en'){
-  keywords = whatTheyAskedUs.match(/why|what|how|where|here|there|place|lake|forest|rhizome|root|grow|ground|move|life|miss|feel|familiar|enjoy|human/ig);
-}
+    keywords = whatTheyAskedUs.match(/miksi|mitä|miten|missä|tänne|tääl|tässä|siellä|paikka|järv|metsä|hytty|mer|rihmasto|juur|kasv|maas|muut|elämä|kaip|kaiv|tunn|tunt|viih|ihmi/ig);
+  } else if (language == 'en'){
+    keywords = whatTheyAskedUs.match(/why|what|how|where|here|there|place|lake|forest|mosquit|ocean|rhizome|root|grow|ground|move|life|miss|feel|familiar|enjoy|human/ig);
+  }
 
   var structure = [];
-  if( whatTheyAskedUs.match(/infinity/ig) ){
-    //if its in infinity mode: create a new structure by making a HUUUUUGE array:
-    for(let inf = 0; inf < 88; inf++){
+  if( whatTheyAskedUs.match(/8infinity8/ig) ){
+    //if it is in infinity mode: create a new random structure of 8:
+    for(let inf = 0; inf < 8; inf++){
       let randomCategory = random(structures[0]);
       structure.push( randomCategory );
     }
@@ -225,6 +247,7 @@ function museAboutBeingTransplanted(whatTheyAskedUs){
 
   //selects some thoughts to playback based on these keywords:
   var selections = selectThoughts(keywords, structure);
+  //fades out the transcript of the persons speech:
   $("#questions").fadeOut( 2000 );
   play(selections);
 
@@ -232,15 +255,13 @@ function museAboutBeingTransplanted(whatTheyAskedUs){
 
 function selectThoughts(keywords, structure){
 
-  //console.log("this is the chosen structure:", structure);
-
   //for the first element, find concordance with keywords
   //for the following ones, choose random ones
   var l, j, k, selections = [], matches = [];
   for(l=0; l < structure.length; l++){
     //if I pass it keywords=null and structure = [], then it will do: selections.push(random(i)) == random interjection..
     if(l==0){ //choose the first random musing so that it matches a keyword in the users speech
-      if(keywords == null || keywords[0] == 'infinity'){
+      if(keywords == null || keywords[0] == '8infinity8'){
         //if the system doesn't recognise any keywords,
         //just choose an interjection (a small response, a musing... but no huge response):
         //OR if its in inifity mode: just push a random element
@@ -251,28 +272,22 @@ function selectThoughts(keywords, structure){
       } else { //find all the sentences where a keyword occurs
           for(j=0; j<structure[l].length; j++){
             for(k=0; k<keywords.length; k++){
-              //if(!subtitles){
-                if(language == 'fi'){
-                if(structure[l][j].text.includes(keywords[k]) ){
-                //this is pushing ALL the matches!!!!
-                matches.push(structure[l][j]);
+                  if(language == 'fi'){
+                    if(structure[l][j].text.includes(keywords[k]) ){
+                    //push all the matches:
+                    matches.push(structure[l][j]);
+                }
+              } else if (language == 'en'){
+                  if(structure[l][j].subtitleEN.includes(keywords[k]) ){
+                  //push all the matches:
+                  matches.push(structure[l][j]);
+                }
               }
-            //} else if (subtitles){
-            } else if (language == 'en'){
-                if(structure[l][j].subtitleEN.includes(keywords[k]) ){
-                //this is pushing ALL the matches!!!!
-                matches.push(structure[l][j]);
-              }
-            }
             }
           }
-          if(matches.length === 0){ //if there isn't any match:
-            //what if there isn't a match? -> then we just don't add an element that matches?
-            //if there IS NO MATCH in this group of either (a, i, q), we just add a random element from this group??
-            //result: no concordance between what the user says and what the system responds..
+          if(matches.length === 0){ //if there isn't any match, just push a random element:
             selections.push(random(structure[l]));
           } else { //select a random sentence from the matches:
-            //console.log("These are the matches that I found: ", matches);
             selections.push(random(matches));
           }
       }
@@ -287,29 +302,28 @@ function selectThoughts(keywords, structure){
   return selections;
 }
 
-function play(selections){ //this is not going to return a deferred, because our button is always going to be enabled...
+//Credits of this method go to TNT, courtesy of Tero Marttila (August of 2016)
+//The code in play() is copied from Give me a Reason, which was written by
+//Tero Marttila in August of 2016 when we refactored my otherwise functional code:
+//of course it has been adapted here to the necessities of this work:
+function play(selections){
   var audioDiv = $('#audio');
   audioDiv.empty();
-  //$("#answers").stop(true, true).empty().hide(); //perhaps this
-  $("#answers").hide();
   var loading = 0;
   var playing = 0;
 
   function playNext(){
     audios[playing].play();
-    //if(subtitles){
     if(language == 'en'){
       $("#answers").show();
       $("#answers").append("<span>" + audios[playing].getAttribute('subtitleEN') + "</span>", "<br>");
 
       if(infinity){
-        //once 8 lines are displayed, remove the first one:
+        //once 8 lines are displayed, remove the first one (and thereby "scroll" all lines upwards):
         if($("#answers").children().length == 18 ){
           $("#answers").children().slice(0, 2).remove();
         }
-      }
-
-      if(!infinity){
+      } else { //if not in infinity mode, fade out answers/lines:
         $("#answers").fadeOut( 40000 , () => $("#answers").hide() );
       }
     }
@@ -322,7 +336,7 @@ function play(selections){ //this is not going to return a deferred, because our
 
     //set infinity button text content to 8 while the audio files are loading:
     if(loading == 1){
-      infinityButton.html('⧖'); //⧖ ⧖ ⧖ ⏱  ◴◵◶ ◷◶◵◴ &#x23f3
+      infinityButton.html('⧖'); //alternatives: ⧖ ⏱ ◷◶◵◴
     }
 
     var audio = $( "<audio></audio>", {
@@ -332,12 +346,10 @@ function play(selections){ //this is not going to return a deferred, because our
       preload: 'auto',
       on: {
         canplaythrough: function(event){
-          //console.log("loading is:", loading);
-          // 88 -> 1
           loading--;
           if(loading === 0){
             playNext();
-            infinityButton.html('∞');
+            infinityButton.html('∞'); //set button back to ∞ ones all audio loaded:
           }
         },//close canplaythrough:
         ended: function(event){
@@ -345,8 +357,10 @@ function play(selections){ //this is not going to return a deferred, because our
           if(playing<audios.length){
             playNext();
           } else {
-            //$("#answers").stop(true, true).empty().hide();
-            //deferred.resolve(); //this would now be used to reactivate the input button
+              if(infinity){//if in infinity mode mode:
+                //once all have been played: start a new cycle of infinity (ie. 8 musings):
+                museAboutBeingTransplanted("8infinity8");
+              }
           }
         }//close ended:
       }//close on:
--- transplanted.js
+++ transplanted.js
@@ -1,20 +1,18 @@
-  var listener, canvas, heartButton, speakingToMyHeart, infoButton, finnishButton, englishButton, infinityButton, url, language, subtitles, thoughts;
+  var listener;
   var a = [], i = [], q = [], structures = [];
-  let isSpeechRecognitionSupported = false;
+  let isSpeechRecognitionSupported = false; //false until proven true
 
 function preload() {
 
-  language = 'fi'; //or set this using a button when you support more languages
-  subtitles = false;
+  language = 'fi'; //set to Finnish by default, but possible to change to english using the "en"-button
 
-  //var isSpeechRecognitionSupported = false;
   try {
     //disable all of this if window doesn't have webkitSpeechRecognition or SpeechRecognition
     window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
     listener = new SpeechRecognition;
     isSpeechRecognitionSupported = true;
   } catch (err) {
-    console.log("Speech recognition is not supported");
+    //console.log("Speech recognition is not supported");
     window.alert("Please view this work in Chrome");
   }
 
@@ -22,22 +20,22 @@ function preload() {
 
         window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
         listener = new SpeechRecognition;
-        listener.lang = 'fi-FI';
+        listener.lang = 'fi-FI'; //set to Finnish by default
         listener.interimResults = true;
         listener.continuous = true;
 
         listener.onresult = (event) => {
           speakingToMyHeart = event.results[0][0].transcript;
+          //display transcript on screen:
           $("#questions").html('" <em>' + speakingToMyHeart.toLowerCase() + '<em> ? "');
           if(event.results[0].isFinal){
-            //museAboutBeingTransplanted based on what the user spoke to the heart
+            //based on what the user spoke to the heart, museAboutBeingTransplanted:
             museAboutBeingTransplanted(speakingToMyHeart.toLowerCase());
             stopAndClear();
-            //disable button while transplanted is responding? :
           }
         }
 
-
+      //in p5/preload() this will load before going into setup:
       url = 'assets/audio/' + language + '/' + 'thoughts.json';
       thoughts = loadJSON(url);
 
@@ -46,16 +44,26 @@ function preload() {
 }//end preload()
 
 function setup(){
-  //these are all arrays of snippets:
+  //these are all arrays of audio snippets:
   a = thoughts.answers;
   i = thoughts.interjections;
   q = thoughts.questions;
   //this is in turn an array of arrays:
+  //ie. different structures for the musings:
   structures = [
+    [a, q, i],
+    [a, a, i],
+    [a, i, a, a, a, q],
+    [a, a, a, i, q, i],
+    [i, a, a, a, a, q, i, a, a, q],
+    [i, q, a, a],
+    [i, i, a, q],
+    [i, i, i, q],
+    [i, a, a, i],
     [q, a, i],
-    [q, a, a, i, a, q],
+    [q, a, a, a, i, a, q],
     [q, i, q],
-    [q, i, a, a, i, q],
+    [q, i, a, a, a, i, q],
     [q, q, i]
   ];
   if (isSpeechRecognitionSupported){
@@ -64,4 +72,7 @@ function setup(){
 } //close setup
 
 function draw(){
+  //oops, didn't make any use of this... BUT preload() was useful! as are
+  //other p5.js methods, such as random()
+  //this is me, cherrypicking from HTML5, p5.js and jquery :D
 }
